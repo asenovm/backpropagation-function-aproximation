ba0eedac50b313f6a8a6f0252607728643edb499
asenovm <asenov.m@gmail.com>
2014-05-21 00:40:35 +0300

    * Extracted the main frame into a separate class

--- src/edu/fmi/nn/backpropagation/FunctionApproximation.java
+++ src/edu/fmi/nn/backpropagation/FunctionApproximation.java
@@ -1,9 +1,8 @@
 package edu.fmi.nn.backpropagation;
 
-import java.awt.Dimension;
-
-import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+
+import edu.fmi.nn.backpropagation.view.FunctionFrame;
 
 public class FunctionApproximation {
 	public static void main(String[] args) {
@@ -11,15 +10,7 @@ public class FunctionApproximation {
 
 			@Override
 			public void run() {
-				final JFrame frame = new JFrame("Function Approximation");
-				
-				final Dimension size = new Dimension(800, 600);
-				frame.setMinimumSize(size);
-				frame.setMaximumSize(size);
-				frame.setPreferredSize(size);
-				
-				frame.pack();
-				frame.setVisible(true);
+				new FunctionFrame("Function Approximation");
 			}
 		});
 	}
