c8f9d1882352a94b6efe67e5249372cd55ea2487
asenovm <asenov.m@gmail.com>
2014-06-20 22:57:13 +0300

    * added configuration fields for the neural network

--- src/edu/fmi/nn/backpropagation/view/MenuPanel.java
+++ src/edu/fmi/nn/backpropagation/view/MenuPanel.java
@@ -8,7 +8,9 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class MenuPanel extends JPanel {
 
@@ -76,11 +78,29 @@ public class MenuPanel extends JPanel {
 		setMaximumSize(dimension);
 		setPreferredSize(dimension);
 
+		setBackground(Color.GRAY);
+
+		createAndAddConfigurationField("Hidden Units Count:", "6", 40, 20);
+		createAndAddConfigurationField("Learning Rate:", "0.01", 40, 20);
+		createAndAddConfigurationField("Momentum", "0.1", 40, 20);
+		createAndAddConfigurationField("Number of epochs:", "1000000", 80, 20);
+
 		approximateButton = createAndAddButton(TEXT_APPROXIMATE,
 				new ApproximateOnMouseListener());
 		clearButton = createAndAddButton(TEXT_CLEAR, new ClearOnMouseListener());
+	}
 
-		setBackground(Color.GRAY);
+	public void createAndAddConfigurationField(final String label,
+			final String defaultValue, final int width, final int height) {
+		final JLabel epochs = new JLabel(label);
+		final JTextArea epochsArea = new JTextArea(defaultValue);
+		final Dimension epochsFieldDimension = new Dimension(width, height);
+		epochsArea.setPreferredSize(epochsFieldDimension);
+		epochsArea.setMinimumSize(epochsFieldDimension);
+		epochsArea.setMaximumSize(epochsFieldDimension);
+		add(epochs);
+		add(epochsArea);
+
 	}
 
 	public MenuPanel() {
@@ -88,7 +108,7 @@ public class MenuPanel extends JPanel {
 	}
 
 	public MenuPanel(boolean isDoubleBuffered) {
-		this(new FlowLayout(FlowLayout.CENTER), isDoubleBuffered);
+		this(new FlowLayout(FlowLayout.CENTER, 10, 20), isDoubleBuffered);
 	}
 
 	public MenuPanel(LayoutManager layout) {
